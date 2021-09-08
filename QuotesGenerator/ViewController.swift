7dbe80e3891ceefcdd29d65dade1f6444c75ad11
sj-lee98 <denny00123@naver.com>
2021-09-08 16:41:45 +0900

    version3

--- QuotesGenerator/ViewController.swift
+++ QuotesGenerator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     
-    let quotes = [ // 6개
+    let quotes = [ // 6개...
         Quote(contents: "죽음을 두려워하는 나머지 삶을 시작조차 못하는 사람이 많다.", name: "벤다이크"),
         Quote(contents: "나는 나 자신을 빼고 모두 안다.", name: "비용"),
         Quote(contents: "편견이란 실효성이 없는 의견이다.", name: "암브로스 빌"),
